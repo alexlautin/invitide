85bce8da7da6e6f7078073f58e931c36052dfc66
alexlautin <alexalautin@gmail.com>
2025-04-09 18:37:29 -0400

    initialize supabase

---
+++ utils/supabase/server.ts
@@ -0,0 +1,10 @@
+'use server'
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export const createClient = async () => {
+  return createServerComponentClient({
+    cookies,
+  })
+}
